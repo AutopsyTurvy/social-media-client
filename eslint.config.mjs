ca0217558aea680eed65f686fe45f9dbec161c9c
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2024-08-14 15:06:11 +0200

    Testing ESLint and Prettier integration

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
+  pluginJs.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+];
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
-import router from "./router/index.js";
-import ui from "./ui/index.js";
+//index.js
+
+import router from './router/index.js';
+import ui from './ui/index.js';
 
 ui();
 router();
+
+
+
+// testing eslint and prettier
+var testing = 'testing'
+console.log(testing)
