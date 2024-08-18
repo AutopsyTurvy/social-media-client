7095ac600fa0714b53d666b1a0c0b8356b7ff531
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2024-08-18 18:44:21 +0200

    finished unit testing- made sure that eslint is configured on commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,22 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-var': 'error',
+      'prefer-const': 'error',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'no-unused-vars': 'warn',
+      eqeqeq: ['error', 'always'],
+    },
+  },
 ];
